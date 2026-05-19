8e523454390efcb19634d77c07795de30219ba70
Jonathan Kizer <jk@jonathankizer.com>
2026-05-18 19:26:55 -0700

    Initial Commit

---
+++ dbtdagviewer/dbtdagviewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  dbtdagviewerApp.swift
+//  dbtdagviewer
+//
+//  Created by Jonathan Kizer on 5/18/26.
+//
+
+import SwiftUI
+
+@main
+struct dbtdagviewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
